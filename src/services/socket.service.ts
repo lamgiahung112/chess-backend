72bf67442aa61f67450b9697b27f1737d1267b35
Hung Lam <hung.lam@redharegames.com>
2022-07-02 19:19:31 +0700

    add socket events

--- src/services/socket.service.ts
+++ src/services/socket.service.ts
@@ -1,9 +1,33 @@
 import { Server, Socket } from "socket.io"
+import { v4 } from "uuid"
 import { EVENTS } from "../configs/Socket.config"
+
+const rooms: Record<string, { name: string; participants: number }> = {}
 
 const socket = (io: Server) => {
     io.on(EVENTS.connection, (socket: Socket) => {
-        console.log(socket.id)
+        console.log(`User connected with id: ${socket.id}`)
+
+        socket.on(EVENTS.CLIENT.CREATE_ROOM, ({ roomName }) => {
+            console.log(`Created room name: ${roomName}`)
+
+            // Create room ID
+            const roomID = v4()
+
+            // Add room ID to rooms oject
+            rooms[roomID] = {
+                name: roomName,
+                participants: 1,
+            }
+
+            // client is subscribed to new room
+            socket.join(roomID)
+
+            // emit to everyone there's a new room
+            socket.broadcast.emit(EVENTS.SERVER.ROOMS, rooms)
+            socket.emit(EVENTS.SERVER.ROOMS, rooms)
+            socket.emit(EVENTS.SERVER.JOINED_ROOM, roomID)
+        })
     })
 }
 
