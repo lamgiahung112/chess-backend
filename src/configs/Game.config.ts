577e8b8e915e681733e78ad9b290a5d140345d9e
Hung Lam <hung.lam@redharegames.com>
2022-07-05 18:49:02 +0700

    almost complete playability

---
+++ src/configs/Game.config.ts
@@ -0,0 +1,74 @@
+import { type } from "os"
+
+const PIECE = {
+    // Pawns
+    BP: "BP",
+    WP: "WP",
+    // Rooks
+    BR: "BR",
+    WR: "WR",
+    // Knights
+    BKn: "BKn",
+    WKn: "WKn",
+    // Bishops
+    BB: "BB",
+    WB: "WB",
+    // Queen
+    BQ: "BQ",
+    WQ: "WQ",
+    // King
+    BK: "BK",
+    WK: "WK",
+}
+
+interface IGameData {
+    board: {
+        [num: number]: string
+    }
+    turn: "W" | "B"
+}
+
+const gameInitialState: IGameData = {
+    board: {
+        // White side
+        11: PIECE.WR,
+        12: PIECE.WKn,
+        13: PIECE.WB,
+        14: PIECE.WQ,
+        15: PIECE.WK,
+        16: PIECE.WB,
+        17: PIECE.WKn,
+        18: PIECE.WR,
+        //
+        21: PIECE.WP,
+        22: PIECE.WP,
+        23: PIECE.WP,
+        24: PIECE.WP,
+        25: PIECE.WP,
+        26: PIECE.WP,
+        27: PIECE.WP,
+        28: PIECE.WP,
+        // Black side
+        71: PIECE.BP,
+        72: PIECE.BP,
+        73: PIECE.BP,
+        74: PIECE.BP,
+        75: PIECE.BP,
+        76: PIECE.BP,
+        77: PIECE.BP,
+        78: PIECE.BP,
+        //
+        81: PIECE.BR,
+        82: PIECE.BKn,
+        83: PIECE.BB,
+        84: PIECE.BQ,
+        85: PIECE.BK,
+        86: PIECE.BB,
+        87: PIECE.BKn,
+        88: PIECE.BR,
+    },
+    turn: "B",
+}
+
+export type { IGameData }
+export { gameInitialState, PIECE }
--- src/services/socket.service.ts
+++ src/services/socket.service.ts
@@ -1,8 +1,12 @@
 import { Server, Socket } from "socket.io"
 import { v4 } from "uuid"
 import { EVENTS } from "../configs/Socket.config"
+import { IGameData } from "../configs/Game.config"
 
-const rooms: Record<string, { name: string; participants: number }> = {}
+const rooms: Record<
+    string,
+    { name: string; participants: number; hasWhiteSide: boolean }
+> = {}
 
 const socket = (io: Server) => {
     io.on(EVENTS.connection, (socket: Socket) => {
@@ -18,6 +22,7 @@ const socket = (io: Server) => {
             rooms[roomID] = {
                 name: roomName,
                 participants: 1,
+                hasWhiteSide: true,
             }
 
             socket.join(roomID)
@@ -26,24 +31,55 @@ const socket = (io: Server) => {
             socket.broadcast.emit(EVENTS.SERVER.ROOMS, rooms)
             socket.emit(EVENTS.SERVER.ROOMS, rooms)
 
-            socket.emit(EVENTS.SERVER.JOINED_ROOM, roomID)
+            socket.emit(EVENTS.SERVER.JOINED_ROOM, { roomID, side: "W" })
         })
 
         socket.on(EVENTS.CLIENT.JOIN_ROOM, ({ _roomID, username }) => {
+            if (!rooms[_roomID] || rooms[_roomID].participants >= 2) return
             console.log(
                 `User with id ${socket.id} joined room: ${rooms[_roomID].name}`
             )
+            rooms[_roomID].participants++
 
             socket.join(_roomID)
-            socket.emit(EVENTS.SERVER.JOINED_ROOM, _roomID)
+            socket.emit(EVENTS.SERVER.ROOMS, rooms)
+            socket.emit(EVENTS.SERVER.JOINED_ROOM, {
+                roomID: _roomID,
+                side: rooms[_roomID].hasWhiteSide ? "B" : "W",
+            })
             socket.broadcast.emit(EVENTS.SERVER.PLAYER_JOIN_CLIENT_ROOM, {
                 username,
             })
         })
 
-        socket.on(EVENTS.CLIENT.LEAVE_ROOM, ({ _roomID }) => {
-            socket.leave(_roomID)
+        socket.on(EVENTS.CLIENT.REFRESH_ROOM, () => {
+            socket.emit(EVENTS.SERVER.ROOMS, rooms)
         })
+
+        socket.on(EVENTS.CLIENT.LEAVE_ROOM, ({ _roomID, side }) => {
+            socket.leave(_roomID)
+            rooms[_roomID].participants--
+            rooms[_roomID].hasWhiteSide =
+                rooms[_roomID].hasWhiteSide && side !== "W"
+
+            if (rooms[_roomID].participants === 0) {
+                delete rooms[_roomID]
+            }
+
+            socket.broadcast.emit(EVENTS.SERVER.ROOMS, rooms)
+            socket.emit(EVENTS.SERVER.ROOMS, rooms)
+        })
+
+        // on Client move, change side and send gamedata back to the other client
+        socket.on(
+            EVENTS.CLIENT.MOVE,
+            ({ room, gameData }: { room: string; gameData: IGameData }) => {
+                socket.to(room).emit(EVENTS.SERVER.UPDATE_GAME_DATA, gameData)
+                socket.broadcast
+                    .to(room)
+                    .emit(EVENTS.SERVER.UPDATE_GAME_DATA, gameData)
+            }
+        )
     })
 }
 
